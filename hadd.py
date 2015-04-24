7f626122133a1e56b84978977e4b3d8c75129b8b
luca <luca.martini82@gmail.com>
2015-04-24 18:34:19 +0200

    forgotten

--- hadd.py
+++ hadd.py
@@ -3,16 +3,16 @@
 import os
 import ROOT
 
-os.system(r'hadd -f ./plots/pat_xy_h.root ./plots/pat_xy_h_*.root')
-os.system(r'hadd -f ./plots/pat_zr_h.root ./plots/pat_zr_h_*.root')
+os.system(r'hadd -f ./plots/pat_xy_h.root ./plots_temp/pat_xy_h_*.root')
+os.system(r'hadd -f ./plots/pat_zr_h.root ./plots_temp/pat_zr_h_*.root')
 
-os.system(r'mv ./plots/sc_xy_h_0.root ./plots/sc_xy_h.root')
-os.system(r'mv ./plots/sc_xy_h_0.pdf ./plots/sc_xy_h.pdf')
-os.system(r'mv ./plots/sc_zr_h_0.root ./plots/sc_zr_h.root')
-os.system(r'mv ./plots/sc_zr_h_0.pdf ./plots/sc_zr_h.pdf')
+os.system(r'mv ./plots_temp/sc_xy_h_0.root ./plots/sc_xy_h.root')
+os.system(r'mv ./plots_temp/sc_xy_h_0.pdf ./plots/sc_xy_h.pdf')
+os.system(r'mv ./plots_temp/sc_zr_h_0.root ./plots/sc_zr_h.root')
+os.system(r'mv ./plots_temp/sc_zr_h_0.pdf ./plots/sc_zr_h.pdf')
 
-os.system(r'mv ./plots/pat_*_h_*.* ./temp/')
-os.system(r'mv ./plots/sc_*_h_*.* ./temp/')
+os.system(r'mv ./plots_temp/pat_*_h_*.* ./temp/')
+os.system(r'mv ./plots_temp/sc_*_h_*.* ./temp/')
 
 i = 0
 for filename in os.listdir("./plots"):
