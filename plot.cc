498e2de585ad39256b6e24ae3801ae83143eae11
luca <luca.martini82@gmail.com>
2015-04-30 13:00:57 +0200

    fix default dir

--- plot.cc
+++ plot.cc
@@ -1,20 +1,22 @@
 #include "plot.h"
 
-plot::plot(int x, int y) : tail("") {
+plot::plot(int x, int y) : tail(""), dir("./") {
   c = new TCanvas("c", "c", x, y);
 }
 
 plot::~plot() {
-  delete c;
+  // delete c;
 }
 
 void plot::plot1D(TH1D * h) {
+  c->cd();
   h->Draw();
   c->Print(Form("%s/%s%s.pdf", dir.c_str(), h->GetName(), tail.c_str()));
   h->SaveAs(Form("%s/%s%s.root", dir.c_str(), h->GetName(), tail.c_str()));
 }
 
 void plot::plot2D(TH2D * h) {
+  c->cd();
   h->SetStats(0);
   h->Draw("COL");
   c->Print(Form("%s/%s%s.pdf", dir.c_str(), h->GetName(), tail.c_str()));
